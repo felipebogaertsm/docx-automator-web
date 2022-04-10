b49738fe35672e9ce2beea6cd09303aad5bad6c0
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-10 11:04:30 -0300

    0.1.1
    Added json read functionaliity

--- core/main.py
+++ core/main.py
@@ -1,7 +1,11 @@
 from docxtpl import DocxTemplate
+import json
 
 if __name__ == '__main__':
     document = DocxTemplate('../samples/template_ctx.docx')
-    context = {'device_name': 'Hi'}
-    document.render(context)
+    
+    with open('../samples/data.json') as data_file:
+        context = json.load(data_file) # getting context from json file
+
+    document.render(context)  # replacing jinja2 tags with json context
     document.save()
