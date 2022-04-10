f108e3f846da6bc3be0e0f16e438325346be99f7
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-10 13:12:08 -0300

    0.1.2

--- core/main.py
+++ core/main.py
@@ -1,11 +1,28 @@
-from docxtpl import DocxTemplate
+import datetime
 import json
 
-if __name__ == '__main__':
-    document = DocxTemplate('../samples/template_ctx.docx')
-    
-    with open('../samples/data.json') as data_file:
-        context = json.load(data_file) # getting context from json file
+from docxtpl import DocxTemplate, InlineImage, RichText
+from docx.shared import Mm
+
+
+def get_datetime_ctx():
+    date = datetime.datetime.now()
+    return {"date": {"day": date.day, "month": date.strftime("%B"), "year": date.year}}
+
+
+if __name__ == "__main__":
+    document = DocxTemplate("../samples/template.docx")
+    rt = RichText()
+
+    with open("../samples/data.json") as data_file:
+        context = json.load(data_file)  # getting context from json file
+
+    # Replacing image:
+    context["image1"]["src"] = InlineImage(
+        document, context["image1"]["src"], width=Mm(150)
+    )
+
+    context = {**context, **get_datetime_ctx()}
 
     document.render(context)  # replacing jinja2 tags with json context
-    document.save()
+    document.save("../samples/template_new.docx")
